7bdb773498aa9461447abd5244331b57565429f7
Bhupendra <sainibhupendra50042@gmail.com>
2024-12-09 14:13:56 +0530

    home.jsx file in some content changed

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,13 +5,16 @@ import resume from "../../public/resimg.jpg";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  const authlogin = localStorage.getItem("learninglogin");
+
   return (
     <div className="flex flex-col lg:flex-row p-10 bg-green-50 min-h-screen justify-center items-center space-y-10 lg:space-y-0 lg:space-x-10">
       {/* Book Section */}
       <div className="w-full lg:w-1/2 flex flex-col items-center space-y-4">
         <img src={book} alt="book" className="w-40 h-auto sm:w-52" />
         <Link
-          to="/addbooks"
+          // to="/addbooks"
+          to={authlogin ? "/addbooks" : "/"}
           className="text-lg sm:text-xl font-bold text-blue-700 underline hover:text-blue-950"
         >
           Add Book
@@ -22,7 +25,7 @@ const Home = () => {
       <div className="w-full lg:w-1/2 flex flex-col items-center space-y-4">
         <img src={resume} alt="resume" className="w-36 h-auto sm:w-44" />
         <Link
-          to="/createresume"
+          to={authlogin ? "/createresume" : "/"}
           className="text-lg sm:text-xl font-bold text-blue-700 underline hover:text-blue-950"
         >
           Create Resume
